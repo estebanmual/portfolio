891ca504b3c81b667b172f71e369d4bd2cefbd07
Estebanmual <esteban.munozal@gmail.com>
2022-08-04 13:00:25 -0500

    Added home section style

--- src/components/sections/Home/Home.jsx
+++ src/components/sections/Home/Home.jsx
@@ -6,26 +6,30 @@ import Medium from '../../../assets/social_icons/medium.svg'
 import Twitter from '../../../assets/social_icons/twitter.svg'
 import Instagram from '../../../assets/social_icons/instagram.svg'
 import style from './Home.module.css'
+import Globe from '../../../assets/globe.svg'
+import Dots from '../../../assets/dots.svg'
 
 function Home() {
   return (
-    <div className={style.home}>
+    <section className={style.homeSection}>
+      <img src={Globe} alt="" className={style.globeImage}/>
       <div>
         <p>HELLO WORLD.</p>
         <h1>I'm Esteban Munoz</h1>
         <p>| FULL-STACK DEVELOPER |</p>
       </div>
-      <button>MORE ABOUT ME</button>
+      <button className={style.moreButton}>MORE ABOUT ME</button>
 
-      <div>
-        <a href="#"><img src={Github} alt="" /></a>
-        <a href="#"><img src={Linkedin} alt="" /></a>
-        <a href="#"><img src={Medium} alt="" /></a>
-        <a href="#"><img src={Twitter} alt="" /></a>
-        <a href="#"><img src={Angelist} alt="" /></a>
-        <a href="#"><img src={Instagram} alt="" /></a>
+      <div className={style.socialIcons}>
+        <a href="#"><img src={Github} alt="Github" /></a>
+        <a href="#"><img src={Linkedin} alt="Linkedin" /></a>
+        <a href="#"><img src={Medium} alt="Medium" /></a>
+        <a href="#"><img src={Twitter} alt="Twitter" /></a>
+        <a href="#"><img src={Angelist} alt="Angellist" /></a>
+        <a href="#"><img src={Instagram} alt="Instagram" /></a>
       </div>
-    </div>
+      <img src={Dots} alt="" className={style.dotsImage}/>
+    </section>
   )
 }
 
